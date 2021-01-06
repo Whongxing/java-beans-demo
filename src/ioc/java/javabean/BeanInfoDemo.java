3a75c4fd175b206e0cfc03907d2e78c29aa852ee
红星 <1319413075@qq.com>
2021-01-06 18:41:25 +0800

    【描述】  提交图片

--- src/ioc/java/javabean/BeanInfoDemo.java
+++ src/ioc/java/javabean/BeanInfoDemo.java
@@ -1,11 +1,17 @@
 package ioc.java.javabean;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+
 /**
  * BeanInfo示例
+ * BeanInfo是JavaBeans里的一个api,用于管理Bean的元信息
  * @author wanghx
  */
 public class BeanInfoDemo {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IntrospectionException {
+        //处理一下,自省异常
+        BeanInfo beanInfo = Introspector.getBeanInfo(Person.class);
     }
 }
